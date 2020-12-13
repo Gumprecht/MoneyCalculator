1f644b759c5d7e404953e53b64d430cdcc5e009c
Andi <Andi@GUMPRECHTS-ENGEL>
2020-12-13 20:09:47 +0000

    MoneyCalculator versión3

--- src/moneycalculator/MoneyCalculator.java
+++ src/moneycalculator/MoneyCalculator.java
@@ -1,61 +1,85 @@
 package moneycalculator;
 
+import moneycalculator.model.Currency;
+import moneycalculator.model.CurrencyList;
+import moneycalculator.model.ExchangeRate;
+import moneycalculator.model.Money;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.URL;
 import java.net.URLConnection;
+import java.time.LocalDate;
+import java.time.Month;
 import java.util.Scanner;
 
 public class MoneyCalculator {
-    double amount;
-    double exchangerate;
-    String currencyFrom;
-    String currencyTo;
-    
-    public static void main(String[] args) throws IOException {
-        MoneyCalculator moneycalculator = new MoneyCalculator();
-        moneycalculator.control();
+    public static void main(String[] args) throws Exception {
+      MoneyCalculator moneyCalculator = new MoneyCalculator();
+      moneyCalculator.execute();       
     }
 
-    private void control() throws IOException {
+    private final CurrencyList currencyList;
+    private Money money;
+    private Currency currencyTo;
+    private ExchangeRate exchangeRate;
+
+    public MoneyCalculator() {
+        this.currencyList = new CurrencyList();
+    }
+    
+    private void execute() throws Exception{
         input();
         process();
         output();
     }
 
-    private void input() {
-        System.out.println("Introduce una cantidad: ");
+    private void input(){
+        System.out.println("Introduzca cantidad");
         Scanner scanner = new Scanner(System.in);
-        amount = scanner.nextDouble();
+        double amount = Double.parseDouble(scanner.next());
         
-        System.out.println("Introduce una divisa inicial: ");
-        currencyFrom = scanner.next();
-        
-        System.out.println("Introduce una divisa final: ");
-        currencyTo = scanner.next();
-    }
+        while (true) {
+            System.out.println("Introduzca código divisa origen");
+            Currency currency = currencyList.get(scanner.next().toUpperCase());
+            money = new Money(amount, currency);
+            if (currency != null) break;
+            System.out.println("Divisa no conocida");
+        }
 
-    private void process() throws IOException {
-        exchangerate = getExchangeRate(currencyFrom,currencyTo);
+        while (true) {
+            System.out.println("Introduzca codigo divisa destino");
+            currencyTo = currencyList.get(scanner.next().toUpperCase());
+            if (currencyTo != null) break;
+            System.out.println("Divisa no conocida");
+        }
     }
-
-    private void output() {
-        System.out.println(amount + " " + currencyFrom + " = " +
-                amount*exchangerate + currencyTo);
+    
+    private void process() throws Exception{
+        exchangeRate = getExchangeRate(money.getCurrency(), currencyTo);
     }
-
-    private static double getExchangeRate(String from, String to) throws IOException {
+    
+    private void output(){
+        System.out.println(money.getAmount() + " " 
+                + money.getCurrency().getSymbol() + " equivalen a "
+                + money.getAmount() * exchangeRate.getRate() + " "
+                + currencyTo.getSymbol());
+    }
+    
+    private static ExchangeRate getExchangeRate(Currency from, Currency to) 
+            throws IOException {
         URL url = 
             new URL("http://free.currencyconverterapi.com/api/v5/convert?q=" +
-                    from + "_" + to + "&compact=y&apiKey=f0fe48bb7e7faed3bae5");
+                    from.getCode() + "_" + to.getCode() + "&compact=y&apiKey=f0fe48bb7e7faed3bae5");
         URLConnection connection = url.openConnection();
         try (BufferedReader reader = 
                 new BufferedReader(
                         new InputStreamReader(connection.getInputStream()))) {
             String line = reader.readLine();
-            String line1 = line.substring(line.indexOf(to)+12, line.indexOf("}"));
-            return Double.parseDouble(line1);
+            String line1 = line.substring(line.indexOf(to.getCode())+12, line.indexOf("}"));
+            return new ExchangeRate(from, to, 
+                    LocalDate.of(2018, Month.SEPTEMBER, 26), 
+                    Double.parseDouble(line1));
         }
     }
 }
